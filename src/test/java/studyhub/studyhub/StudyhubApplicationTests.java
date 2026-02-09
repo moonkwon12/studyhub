be005d9429dd534af943cc146ed8004faae1bc18
moonkwon12 <ok63477@gmail.com>
2026-02-09 14:39:38 +0900

    Initial setting

---
+++ src/test/java/studyhub/studyhub/StudyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package studyhub.studyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
